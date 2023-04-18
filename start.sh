e53abf1a38b43036acdb3d377eb4e61f22ca2664
shreyasEverwell <shreyas@everwell.org>
2023-04-18 20:23:41 +0530

    updated local run script

--- start.sh
+++ start.sh
@@ -1,1 +1,11 @@
+if [[ ! $(lsof -i:5432 -t | wc -l) -gt 0 ]]; then
+	echo "Starting postgres"
+	pg_ctl start > postgres.log 2>&1 &
+fi
+
+while [[ ! $(lsof -i:5432 -t | wc -l) -gt 0 ]];
+do
+	echo "Waiting for postgres"
+done
+
 mvn spring-boot:run
